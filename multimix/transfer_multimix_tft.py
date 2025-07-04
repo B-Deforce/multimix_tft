3734ffa4f34ab942f400dfe34d9760a64e13a26f
Boje Deforce <72612139+B-Deforce@users.noreply.github.com>
2025-07-04 06:56:51 -0500

    Add multimix for transfer learning

---
+++ multimix/transfer_multimix_tft.py
@@ -0,0 +1,24 @@
+from multimix.multimix_tft import MultiMixTFT
+
+
+class MultiMixTFTT(MultiMixTFT):
+    """MultiMixTFTT model allows to use a pretrained MultiMixTFT model
+        and fine-tune it for a specific task with two outputs.
+    Args:
+        pretrained_model_path (str): Path to the pretrained MultiMixTFT model checkpoint.
+
+    """
+
+    def __init__(
+        self,
+        pretrained_model_path: str,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.save_hyperparameters()
+        self.pretrained_tft = MultiMixTFT.load_from_checkpoint(pretrained_model_path)
+        self.pretrained_tft.freeze()
+        self.automatic_optimization = False
+
+        ## Initializing remaining weights
+        self.init_weights()
