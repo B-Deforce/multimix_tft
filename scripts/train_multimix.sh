b07d7f3b9e0468bb916a01f9045f745f166452f7
Boje Deforce <72612139+B-Deforce@users.noreply.github.com>
2025-07-11 10:34:03 -0500

    Add cli arg and path fixes, add helper functions

--- scripts/train_multimix.sh
+++ scripts/train_multimix.sh
@@ -2,8 +2,9 @@
 # run from the root of the project
 set -e
 
-config_path="configs/config_103050_10_4_classification.yml"
+config_path="configs/bdb_306090.yml"
 
 python main.py \
     --config_path "$config_path" \
-    --task "classification" \
+    --task "regression" \
+    --ckpt_name "MultiMix_bdb_306090" \
--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
+import json
+
 import pytorch_lightning as pl
+import yaml
+
 
 def create_ckpt_name_and_logger(ckpt_name):
     """Create a checkpoint name and logger for PyTorch Lightning.
@@ -7,17 +11,28 @@ def create_ckpt_name_and_logger(ckpt_name):
     Returns:
         loggers (list): A list containing the TensorBoard and CSV loggers.
     """
-    
+
     tb_logger = pl.loggers.TensorBoardLogger(
-        save_dir="../lightning_logs",  
-        name="MultiMix_" + ckpt_name,  
+        save_dir="lightning_logs",
+        name="MultiMix_" + ckpt_name,
     )
 
     csv_logger = pl.loggers.CSVLogger(
-        save_dir="../lightning_logs",
+        save_dir="lightning_logs",
         name="MultiMix_" + ckpt_name,
         version=tb_logger.version,  # Use the same version directory as the TensorBoard logger
     )
     loggers = [tb_logger, csv_logger]
     return loggers
 
+
+def get_config(config_path):
+    with open(config_path) as cfg:
+        config = yaml.safe_load(cfg)
+    return config
+
+
+def read_json(json_path):
+    with open(json_path, "r") as file:
+        f = json.load(file)
+    return f
